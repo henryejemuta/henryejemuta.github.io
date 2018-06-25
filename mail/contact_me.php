946b7124707ae9231f5af1cabd13cd947e55d39f
Henry Ejemuta <henryakin15@hotmail.com>
2018-06-25 14:20:58 +0100

    Email submission updated

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,9 +4,9 @@ if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
    empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   (!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)))
    {
-   echo "No arguments Provided!";
+   echo "Please, Kindly fill out all fields and provide a valid email before submitting the form!";
    return false;
    }
    
@@ -16,11 +16,12 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'henry.ejemuta@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "My GitHub Contact Form:  $name";
+$email_body = "You have received a new message from your github profile contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@henryejemuta.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
+   echo "Your form has been submitted successfully, I'll get back to you ASAP!";
 return true;         
 ?>
